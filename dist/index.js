f2700337a23e9f3aab29b445ab6f78b5dc281d6f
Tayyab Abdullah <tabdullah0801@gmail.com>
2024-07-02 19:34:25 +0500

    Tuple Datatype in TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,13 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 20;
-console.log(age);
+let sales = 200600700;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let number = [1, 2, '3'];
+let array1 = [];
+let array2 = [];
+array2[0] = 1;
+let user = [1, 'abc'];
+user.push(1);
+console.log("Users: ", user);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,10 @@ let array2: number[] = [];
 array2[0] = 1;
 // array2[1] = 'abc'; We get error "String is passed."
 
+
+// Tuple DataType : 1, Tayyab
+
+let user: [number, string] = [1, 'abc'];
+user.push(1);
+
+console.log("Users: ", user);
