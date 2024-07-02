3d6fdf7c1d5628fc93bda9a560e372687330d492
Tayyab Abdullah <tabdullah0801@gmail.com>
2024-07-02 22:11:23 +0500

    Making Object more user friendly.

--- src/index.ts
+++ src/index.ts
@@ -45,12 +45,13 @@ console.log(mySize);
 
 // Objects
 
-
-let employee: {
+type Employee = {
     readonly id: number,  //Readonly can be used to only access value
     name: string,
     retire: (date: Date) => void
-} = {
+}
+
+let employee: Employee = {
     id: 1,
     name: 'Tayyab',
     retire: (date: Date) => {
